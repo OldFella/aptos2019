1b64a7f46a7b6138c0235b7ed37cc9d31a627528
wastedsummer <stefan.wezel@student.uni-tuebingen.de>
2019-07-15 18:03:28 +0200

    changed transforms/normalization

--- load_data.py
+++ load_data.py
@@ -12,6 +12,7 @@ from config import DATA_PATH
 
 
 
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
 
 train_transforms = transforms.Compose([
 	transforms.Resize((128,128)),
@@ -19,15 +20,17 @@ train_transforms = transforms.Compose([
 	transforms.RandomHorizontalFlip(0.5),
 	transforms.RandomRotation(360),
 	transforms.ToTensor(),
-	transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+	normalize
 	])
 
 validation_transforms = transforms.Compose([
 	transforms.Resize((128,128)),
 	transforms.ToTensor(),
-	transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+	normalize
 	])
-
+# validation_transforms = transforms.Compose([
+# 	transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+# 	transforms.ToTensor()])
 
 def create_loader(folder, transforms, batch_size = 4):
 	dataset = torchvision.datasets.ImageFolder(
